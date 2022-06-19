0d35fd2640557a2a987a96497b826e38f4086a76
Skippy <steelstone3@gmail.com>
2022-06-19 11:44:06 +0100

    absolute fresh start

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
-
-mod model;
-mod view;
-mod presenter;
-
 fn main() {
-    let app = Application::builder().application_id("org.tasker.todo").build();
 
-    app.connect_activate(build_ui);
-    app.run();
-}
-
-pub fn build_ui(app: &Application) {
-    let builder = gtk::Builder::from_string(include_str!("./ui/tasker.ui"));
-
-    let window: ApplicationWindow = builder.object("tasker").expect("Could not get object `window` from builder.");
-    
-    window.set_application(Some(app));
-    window.present();
 }
